944b001e2d7ce657ed17aa73fb2bfec24e57fb54
Marta Baumane <zbq776@ku.dk>
2022-08-02 15:36:46 +0200

    Plots for Hogsete

--- code/hogsete/data_vizz_hogsete.R
+++ code/hogsete/data_vizz_hogsete.R
@@ -1,66 +1,91 @@
+## fluxes plots for presenting
 
-library(ggplot2)
-library(dplyr)
-library(patchwork) # To display 2 charts together
-library(hrbrthemes)
+#source("code/hogsete/cleaning_hogsete.R")
+source("code/metaturf.R")
 
-# A few constants
-neeColor <- "#69b3a2"
-erColor <- rgb(0.2, 0.6, 0.9, 1)
+# right join the metaturf     ------------------------------------------------
+#(we are adding here the treatments, sites, and so on)
 
-data = flux.virkesland.90
+# This script will be to visualize the cleaned cflux and PAR data
 
-# I am creating here a unique ID for each plot and measurement
-data = data %>% 
+library("scales")
+library("dataDownloader")
+# download raw data
+# download files from OSF ---------------------------------------
+get_file(node = "pk4bg",
+         file = "Three-D_24h-cflux_hogsete_2022.csv",
+         path = "clean_data",
+         remote_path = "C-Flux")
+clean_hogsete <- read_csv("clean_data/Three-D_24h-cflux_hogsete_2022.csv", na = c("#N/A"))
+
+cflux_hogsete <- right_join(
+  cflux_hogsete, metaturf)
+
+## GPP over 24 h  -----------------------------------------------------------
+# filter(type =="ER") %>% 
+# filter(flux>=0) %>% 
+
+FluxPlot_hogsete <- 
+  
+  cflux_hogsete %>% 
+  filter(type != "NEE") %>%
   mutate(
-    uniqueID =
-      case_when(
-        type == "NEE" ~ fluxID,
-        type == "ER" ~ fluxID-1
-  )
-)
+    type = str_replace_all(type, c(
+      "ER" = "Ecosystem Respiration",
+      "GEP" = "Gross Primary Production"
+    ))
+  ) %>% 
+  # ggplot( aes(
+  #  x = datetime, y = flux, group= turfID, color = warming)) +
+  ggplot( aes(
+    x = datetime, y = flux, color = warming, shape=type)) +
+  geom_point() +
+  facet_grid(type ~ ., scales = "free") +
+  geom_smooth(method = "lm",
+              formula = y ~ poly(x, 9),
+              se = TRUE, size = 0.5, fullrange = FALSE) +
+  
+  geom_hline(
+    yintercept = 0, linetype = "dashed", colour = "black") +
+  
+  geom_vline(
+    xintercept = as.numeric(cflux_hogsete$datetime[505]),
+    linetype = 2, colour = "orange", size=1) +
+  geom_vline(
+    xintercept = as.numeric(cflux_hogsete$datetime[81]),
+    linetype = 2, colour = "blue", size=1) +
+  
+  scale_x_datetime(breaks = date_breaks("2 hour"), labels = date_format("%b %d - %H:%M")) +
+  ggtitle("Hogsete (469 m a.s.l.)\nFluctuating Ecosystem Respiration pattern over 24 hours") +
+  ylab("CO2 umol m2 h-1") +
+  xlab("Time of the day (hours)") +
+  theme(strip.text.y = element_text(size = 14, colour = "black"),
+        axis.ticks = element_line(size=1.5), 
+        axis.text.x = element_text(angle = 20, vjust = 0.8, hjust=0.8),
+        axis.title = element_text(size = 16, color ="darkgrey"),
+        #axis.title.x = element_blank(),
+        axis.line = element_line(color = "grey"),
+        axis.text = element_text(size = 12),
+        legend.position = "right",
+        legend.text = element_text(size = 12),
+        legend.title = element_blank(),
+        # legend.title = element_text(size = 8),
+        #legend.key.width = unit(0.4, 'cm'),
+        #panel.grid.major.x = element_blank(), 
+        #panel.grid.major.y = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        panel.grid.minor.y = element_blank(),
+        plot.title = element_text(size=16)) +
+  # panel.background = element_rect(
+  #  fill = 'white', colour = 'grey')) +
+  scale_color_manual(values = c(
+    "ambient" = "#1e90ff",
+    "transplant" = "#ff0800"
+  )) 
 
-NEE_virk_90 <- data %>% 
-   select(fluxID, PARavg,temp_soilavg, turfID, type, datetime, flux, uniqueID) %>% 
-   filter(type=="NEE") %>% 
-   rename(fluxID_NEE = fluxID, PARavg_NEE= PARavg, temp_soilavg_NEE = temp_soilavg, datetime_NEE = datetime)
 
-NEE_virk_90
-
-ER_virk_90 <- data %>% 
-  select(fluxID, PARavg,temp_soilavg, turfID, type, datetime, flux, uniqueID) %>% 
-  filter(type=="ER") %>% 
-  rename(fluxID_ER = fluxID, PARavg_ER= PARavg, temp_soilavg_ER = temp_soilavg, datetime_ER = datetime)
-
-ER_virk_90 
-
-ER_virk_90 <- ER_virk_90 %>% 
-pivot_wider(names_from = type, values_from = flux)
-
-NEE_virk_90 <- NEE_virk_90 %>% 
-  pivot_wider(names_from = type, values_from = flux)
+FluxPlot_hogsete 
 
 
-flux_virk_90 <- merge(NEE_virk_90, ER_virk_90, by = c("turfID", "uniqueID"))
 
 
-flux_virk_90 <- flux_virk_90 %>% 
-  select("turfID", "uniqueID", "fluxID_NEE", "fluxID_ER", "datetime_NEE", "datetime_ER", "PARavg_NEE", "PARavg_ER", "temp_soilavg_NEE", "temp_soilavg_ER", "NEE", "ER")
-
-
-flux_virk_90 <- flux_virk_90 %>% 
-  mutate(
-    GPP = NEE - ER
-  )
-
-plot(x = flux_virk_90$datetime_ER, y = flux_virk_90$GPP, col="red")
-points(x = flux_virk_90$datetime_ER, y = flux_virk_90$ER, col ="red")
-points(x = flux_virk_90$datetime_ER, y = flux_virk_90$NEE, col ="blue")
-
-flux_virk_90 %>% 
-ggplot(aes(x = datetime_NEE, y = GPP)) +
-  geom_line(aes(color = turfID))
-
-flux_virk_90 %>% 
-  ggplot(aes(x = datetime_NEE, y = ER)) +
-  geom_line(aes(color = turfID))
